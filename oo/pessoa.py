8a37bdb7337b701098b32c920d110b739b8ce0b3
Rodrigo Miranda <adnarimcr@gmail.com>
2018-10-13 11:38:29 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=None):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Cesar')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Rodrigo'
-    print(p.nome)
-    print(p.idade)
+    rodrigo = Pessoa(nome='Rodrigo', idade=36)
+    miranda = Pessoa(rodrigo, nome='Miranda', idade=66)
+    print(Pessoa.cumprimentar(miranda))
+    print(id(miranda))
+    print(miranda.cumprimentar())
+    print(miranda.nome)
+    print(miranda.idade)
+    for filho in miranda.filhos:
+        print(filho.nome)
+
